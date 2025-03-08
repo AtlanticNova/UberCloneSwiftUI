c702e0df5c725f47530671ab02675b2cce90a021
AtlanticNova <idiotcolony97@gmail.com>
2025-03-08 20:10:26 +0700

    feat: initialize uber clone project

---
+++ UberCloneSwiftUI/UberCloneSwiftUI/UberCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneSwiftUIApp.swift
+//  UberCloneSwiftUI
+//
+//  Created by Martinus Andika Novanawa on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
