c00d7e0dee1067420b00e6752975a93cbefaea8b
AtlanticNova <idiotcolony97@gmail.com>
2025-03-08 22:08:47 +0700

    feat: add environment object

--- UberCloneSwiftUI/UberCloneSwiftUI/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ UberCloneSwiftUI/UberCloneSwiftUI/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -11,6 +11,7 @@ import MapKit
 class LocationSearchViewModel: NSObject, ObservableObject {
     // MARK: Properties
     @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocation: String?
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
@@ -18,10 +19,16 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         }
     }
 
+    // MARK: Lifecycle
     override init() {
         super.init()
         searchCompleter.delegate = self
         searchCompleter.queryFragment = queryFragment
+    }
+
+    // MARK: Helpers
+    func selectLocation(_ location: String) {
+        self.selectedLocation = location
     }
 }
 
